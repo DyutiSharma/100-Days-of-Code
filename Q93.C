b65c176476b89fd9f960ec8913f85ac20249901d
Dyuti Sharma <Dyuti.21983@stu.upes.ac.in>
2025-11-09 20:15:12 +0530

    create Q93.C

--- Q93.C
+++ Q93.C
@@ -1,36 +1,40 @@
-/*
-Find the longest word in a sentence.
-*/
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char str[1024];
-    char longest[1024];
-    int maxLen = 0, currLen = 0, i, start = 0, maxStart = 0;
+int areAnagrams(char str1[], char str2[]) {
+    int count[256] = {0};
+    int i;
 
-    printf("Enter a sentence: ");
-    scanf(" %[^\n]", str);
+    if (strlen(str1) != strlen(str2))
+        return 0;
 
-    for(i = 0; ; i++) {
-        if(str[i] != ' ' && str[i] != '\0') {
-            if(currLen == 0) start = i;
-            currLen++;
-        } else {
-            if(currLen > maxLen) {
-                maxLen = currLen;
-                maxStart = start;
-            }
-            currLen = 0;
-        }
-        if(str[i] == '\0') break;
+    for (i = 0; str1[i] && str2[i]; i++) {
+        count[(unsigned char)str1[i]]++;
+        count[(unsigned char)str2[i]]--;
     }
 
-    strncpy(longest, str + maxStart, maxLen);
-    longest[maxLen] = '\0';
+    for (i = 0; i < 256; i++) {
+        if (count[i] != 0)
+            return 0;
+    }
+    return 1;
+}
 
-    printf("Longest word: %s\n", longest);
-    printf("Length: %d\n", maxLen);
+int main() {
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = 0;
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = 0;
+
+    if (areAnagrams(str1, str2))
+        printf("The strings are anagrams.\n");
+    else
+        printf("The strings are NOT anagrams.\n");
 
     return 0;
 }
