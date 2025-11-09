60b4b1428a90e77b74bf414cf797d51285fa8dda
Dyuti Sharma <Dyuti.21983@stu.upes.ac.in>
2025-11-09 20:17:49 +0530

    create Q92.C

--- Q92.C
+++ Q92.C
@@ -1,40 +1,29 @@
+// Find the first repeating lowercase alphabet in a string
+
 #include <stdio.h>
 #include <string.h>
 
-int areAnagrams(char str1[], char str2[]) {
-    int count[256] = {0};
+int main() {
+    char str[100];
+    int freq[26] = {0};
     int i;
 
-    if (strlen(str1) != strlen(str2))
-        return 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
 
-    for (i = 0; str1[i] && str2[i]; i++) {
-        count[(unsigned char)str1[i]]++;
-        count[(unsigned char)str2[i]]--;
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = 0;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            freq[str[i] - 'a']++;
+            if (freq[str[i] - 'a'] == 2) {
+                printf("First repeating lowercase alphabet: %c\n", str[i]);
+                return 0;
+            }
+        }
     }
 
-    for (i = 0; i < 256; i++) {
-        if (count[i] != 0)
-            return 0;
-    }
-    return 1;
-}
-
-int main() {
-    char str1[100], str2[100];
-
-    printf("Enter first string: ");
-    fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = 0;
-
-    printf("Enter second string: ");
-    fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = 0;
-
-    if (areAnagrams(str1, str2))
-        printf("The strings are anagrams.\n");
-    else
-        printf("The strings are NOT anagrams.\n");
-
+    printf("No repeating lowercase alphabet found.\n");
     return 0;
 }
